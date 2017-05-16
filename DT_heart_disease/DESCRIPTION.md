7f037f4122948995b44973befa6deeb6fd00ba55
mfratello <m.fratello89@gmail.com>
2017-05-16 21:53:26 +0200

    Decision Tree initial commit

---
+++ DT_heart_disease/DESCRIPTION.md
@@ -0,0 +1,48 @@
+#Dataset Description
+The data used in this example is a subset of the [Cleveland Database][1], widely used as a benchmark for machine learning models.
+The dataset comprises 270 observations of 76 attributes, however, the most common usages are limited to the 13 features described later.
+
+##Features Description
+1. age: age in years 
+2. sex: sex
+3. cp: chest pain type
+	A: typical angina
+	B: atypical angina
+	C: non-anginal pain
+	D: asymptomatic 
+4. trestbps: resting blood pressure (in mm Hg) 
+5. chol: serum cholestoral in mg/dl 
+6. fbs:  fasting blood sugar > 120 mg/dl
+	A: True
+	B: False
+7. restecg: resting electrocardiographic results
+	A: normal
+	B: having ST-T wave abnormality (T wave inversions and/or ST elevation or depression of > 0.05 mV)
+	C: showing probable or definite left ventricular hypertrophy by Estes' criteria 	
+8. thalach: maximum heart rate achieved 
+9. exang: exercise induced angina (yes/no)
+10. oldpeak = ST depression induced by exercise relative to rest 
+11. slope: the slope of the peak exercise ST segment
+	1: upsloping
+	2: flat
+	3: downsloping 
+12. ca: number of major vessels (0-3) colored by flourosopy 
+14. num: diagnosis of heart disease (angiographic disease status)
+	absence: < 50% diameter narrowing
+	presence: > 50% diameter narrowing
+
+[1]: https://archive.ics.uci.edu/ml/datasets/Heart+Disease
+
+#Setup
+The example has been developed and tested using R 3.1.3 and RStudio 1.0.143.
+See the RStudio [website][2] for installation instructions.
+
+## Dependencies installation
+To run the code and reproduce the results the `rpart` and `rattle` packages need to be installed:
+
+```R
+install.packages("rpart")
+install.packages("rattle")
+```
+
+[2]: https://www.rstudio.com/
---
+++ DT_heart_disease/script.R
@@ -0,0 +1,84 @@
+set.seed(1234567) # for reproducibility
+
+#Load the dataset and print summary information
+data = read.table("heart.txt", header=TRUE, dec=".")
+data$slope = factor(data$slope)
+summary(data)
+
+#Randomly split the data into training and validation sets
+split <- sample(c(1, 2), size = nrow(data), replace = TRUE, prob = c(.7, .3))
+data.train = data[split == 1, ]
+data.test = data[split == 2, ]
+
+#Fit an unconstrained decision tree
+library(rpart)
+settings = rpart.control(
+  minsplit = 2,                     #The minimum number of samples required to split an internal node
+	minbucket = 1,                    #The minimum number of samples for a leaf node
+	cp=0                              #At the moment we don't want to prune the tree, 
+                                    #so any increment in the complexity parameter is accepted
+)
+
+tree = rpart(
+	num ~ .,                          #We want to model any relation between the outcome variable and all the features
+	data=data.train,                  #The actual training data
+	method="class",                   #We train a classification tree
+	parms=list(split="gini"),         #We want to use the cross-entropy splitting criterion
+	control=settings
+)
+
+#After training, we evaluate the model on both the training and the test sets
+pred.train = predict(tree, data.train, type="class")
+pred.test = predict(tree, data.test, type="class")
+
+#The confusion matrix allows to visualize the type of errors made by the model
+conf.train = table(data.train$num, pred.train)
+conf.test = table(data.test$num, pred.test)
+
+#Finally, we evaluate the accuracy
+accuracy.train = sum(diag(conf.train))/sum(conf.train)
+accuracy.test = sum(diag(conf.test))/sum(conf.test)
+
+cat("Predictions of the grown Classification Tree for the training set:\n")
+print(conf.train)
+cat(c("Total accuracy: ", round(accuracy.train, 4) * 100, "%"))
+
+cat("Predictions of the grown Classification Tree for the test set:\n")
+print(conf.test)
+cat(c("Total accuracy: ", round(accuracy.test, 4) * 100, "%"))
+
+#Now, we prune the tree to reduce overfitting
+#The functions plotcp and printcp show a graph and a table respectively
+#of the cross-validated complexity parameter value (alpha)
+#for each pruned sub-tree. The best pruned tree is the one that achieves
+#the least cross-validated error (the y-axis of the graph or the xerror in the table)
+plotcp(tree)
+printcp(tree)
+
+#Then we limit the tree at the complexity parameter corresponding to the lowest 
+#cross-validation error
+tree.pruned = prune(tree, cp=0.0096154)
+
+#We evaluate the pruned tree
+pred.train = predict(tree.pruned, data.train, type="class")
+pred.test = predict(tree.pruned, data.test, type="class")
+
+#The confusion matrix allows to visualize the type of errors made by the model
+conf.train = table(data.train$num, pred.train)
+conf.test = table(data.test$num, pred.test)
+
+#Finally, we evaluate the accuracy
+accuracy.train = sum(diag(conf.train))/sum(conf.train)
+accuracy.test = sum(diag(conf.test))/sum(conf.test)
+
+cat("Predictions of the pruned Classification Tree for the training set:\n")
+print(conf.train)
+cat(c("Total accuracy: ", round(accuracy.train, 4) * 100, "%"))
+
+cat("Predictions of the pruned Classification Tree for the test set:\n")
+print(conf.test)
+cat(c("Total accuracy: ", round(accuracy.test, 4) * 100, "%"))
+
+#Rule Extraction
+library(rattle)
+asRules(tree.pruned)
